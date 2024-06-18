fix: force image rotation to have 3 channels

ocr/core/utils.py
@@ -361,7 +361,9 @@ def rotateImage(image, angle):
     MM = np.absolute(M[:, :2])
     size_new = MM @ size_reverse
     M[:, -1] += (size_new - size_reverse) / 2.0
-    return cv2.warpAffine(image, M, tuple(size_new.astype(int)))
+    return cv2.warpAffine(
+        cv2.cvtColor(image, cv2.COLOR_BGRA2BGR), M, tuple(size_new.astype(int))
+    )
 
 
 def add_element(dict, key, value):